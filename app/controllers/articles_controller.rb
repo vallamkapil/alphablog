d4a20a79f66b302e6d7e8d6e2860567b28fdb6b3
vallamkapil <vallamkapil@gmail.com>
2016-01-11 01:08:26 +0000

    Show,Edit buttons added and list all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,14 +9,27 @@ class ArticlesController < ApplicationController
         if @article.save 
             flash[:notice] = "Article was created successfully."
             redirect_to article_path(@article)
-            
         else
             render 'new'
         end
     end
     
     def index
-        
+        @articles = Article.all
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update 
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully."
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
     end
   def show
       @article = Article.find(params[:id])
