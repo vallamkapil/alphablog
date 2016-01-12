8be2e6302c163f95160cd5cc50bde49a80a509fa
vallamkapil <vallamkapil@gmail.com>
2016-01-12 07:14:29 +0000

    add relations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
-    validates :title, presence:true, length: {minimum:3, maximum:10}
-    validates :description, presence:true, length: {minimum:5, maximum:30}
+    belongs_to :user
     
-    
+    validates :title, presence: true, length: {minimum:3, maximum:10}
+    validates :description, presence: true, length: {minimum:5, maximum:30}
+    validates :user_id, presence: true
 end
